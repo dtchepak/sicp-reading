dba96fee89d0e12cf8ef7fdbe747a54e62345c41
David Tchepak <dave@davesquared.net>
2014-09-16 13:04:51 +1000

    chapter 2 intro

--- chapter2.scm
+++ chapter2.scm
@@ -3,3 +3,16 @@
 (define (l x) (load x))
 (define (reload) (load "chapter2.scm"))
 
+; 2 Building Abstractions with Data
+; ===================================
+; Procedures define local evolution of process
+; Compound procedures let us move from local evolution to
+; thinking more abstractly, enhancing our language to describe
+; the problem in abstract terms.
+
+; Compound data lets us do the same thing, elevating conceptual
+; level above primitive data objects.
+; Compound data increase modularity - we can separate how data
+; object is represented from how they are used. => DATA ABSTRACTION
+
+
